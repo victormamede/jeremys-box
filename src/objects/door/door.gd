f9c995f89d8495475b83a07f0d4e7e881cedaf45
Victor Mamede <victoraugustomamede@hotmail.com>
2025-07-06 17:12:54 -0300

    more cinematics

--- src/objects/door/door.gd
+++ src/objects/door/door.gd
@@ -1,5 +1,7 @@
 extends StaticBody2D
 
+
+@export var next_scene: PackedScene
 @export var required_item: Item
 @export var no_item_text: Array[String] = ["Nothing here!"]
 @export var with_item_text: Array[String] = ["I'm free!"]
@@ -7,16 +9,19 @@ extends StaticBody2D
 @onready var _interactable: Interactable = $Interactable
 
 func _ready() -> void:
-    var i: int
+    var e: int
 
-    i = _interactable.interacted.connect(_on_interactable_interacted)
-    assert(i == 0)
+    e = _interactable.interacted.connect(_on_interactable_interacted)
+    assert(e == 0)
 
 func _on_interactable_interacted(interactor: Interactor) -> void:
+    var e: int
     var inventory: Inventory = interactor.player.get_inventory()
 
     if not inventory.has(required_item):
         interactor.player.get_bubble().say(no_item_text)
+        e = get_tree().change_scene_to_packed(next_scene)
+        assert(e == 0)
         return
 
     inventory.remove_item(required_item)
